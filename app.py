f18da2417575b85181061f6acb523800f2b665d9
Mark Mann <mann.brinson@gmail.com>
2021-02-22 11:24:43 -0800

    initial search page at /search

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def home():
 			items = r.json()['results'][:5]
 			items_trend = [{'tmdb_id': m['id'], 
 			            'backdrop_path': m['backdrop_path'],
-			            'title': m['title'],
+			            'name': m['title'],
 			            'release_date': m['release_date']
 			            } for m in items]
 
@@ -101,25 +101,126 @@ def home():
 	# return render_template('home.html', doc=home_doc_resp)
 	# return render_template('xhr_test.html', doc=home_doc_resp)
 
-@app.route('/', methods=['GET'])
+@app.route('/search', methods=['GET'])
 def client_side():
-	return render_template('home.html')
+	class SearchPage():
+	    def __init__(self):
+	        #Supplied Parameters
+	        self.tmdb_key = "2e510746ca28d7041056c7e57108de4c"
+	        self.poster_base = "https:/www.themoviedb.org/t/p/w1280"
+	        self.genre_files = ['genre_movie.json', 'genre_tv.json']
+	        
+	        #Computed Parameters
+	        self.doc = dict()
+	        self.genre_map = self.load_genres()
+	        
+	    def load_genres(self): #called by __init__
+	        '''Loads genre files into memory, given a genre_files object.
+	        genre_files: ['genre_movie.json', 'genre_tv.json']'''
+	        genre_map = dict()
+	        for file in self.genre_files:
+	            content_type = file.split('_')[1].split('.')[0]
+	            fd = open(file, 'r')
+	            data = json.loads(fd.readline())
 
-	
+	            #Construct dict of key=id, value=name and append to genre_map
+	            g_dict = dict()
+	            for kv_pair in data['genres']:
+	                g_dict[kv_pair['id']] = kv_pair['name']
+	            genre_map[content_type] = g_dict
+	        return genre_map
+	        
+	        
+	    def get_content(self, search_terms, content_type):
+	        '''Gets content based off user content type and search terms.
+	        search_terms - a string containing one or more words
+	        content_type - "tv", "movie" '''
+	        poster_base = "https:/www.themoviedb.org/t/p/w1280"
+
+	        #Check if multiple search terms
+	        terms_list = search_terms.split(' ')
+	        if len(terms_list) >= 2: search_terms2 = "%20".join(terms_list)
+	        else: search_terms2 = search_terms
+
+	        endpoint = f"https://api.themoviedb.org/3/search/{content_type}?api_key={self.tmdb_key}&language=en-US&query={search_terms2}&include_adult=false"
+	        r = requests.get(endpoint)
+
+	        #Extract necesssary features
+	        items = r.json()['results'][:10]
+	        
+	        #Check if feature exists, before extracting
+	        desired_feats = [('id', 'i'), ('title', 's'), ('overview', 's'), ('poster_path', 'path'),
+	                        ('release_date', 's'), ('vote_average', 'f'), ('vote_count', 'i'), ('genre_ids', 'l')]
+	        type_null_map = {'s': 'Not Available', 'i': 0, 'l': 'Not Available', 'f': 0,
+	                        'path': 'https://cinemaone.net/images/movie_placeholder.png'}
+	        items_extract = list()
+	        for idx, m in enumerate(items):
+	            record = dict()
+	            for feat in desired_feats:
+	                if feat[0] not in m:
+	#                     print(f'feat not found: {idx} {feat}')
+	                    record[feat[0]] = type_null_map[feat[1]]
+	                else:
+	                    if m[feat[0]] == None: record[feat[0]] = type_null_map[feat[1]]
+	                    else: record[feat[0]] = m[feat[0]]
+	            items_extract.append(record)
+	#         print(len(items_extract))
+	#         print(items_extract)
+
+	        #Feature engineering
+	        for idx, m in enumerate(items_extract):
+	            #Construct poster path, and add back to items_extract
+	            poster_path = m['poster_path']
+	#             print(poster_path)
+	            if poster_path.split('/')[-1] == 'movie_placeholder.png': pass
+	            else:
+	                poster_full = f'{poster_base}{poster_path}'
+	                items_extract[idx]['poster_path'] = poster_full
+
+	            #Get the genre names
+	            if m['genre_ids'] == 'Not Available': pass
+	            else:
+	                g_names = list()
+	                for g_id in m['genre_ids']:
+	                    g_name = self.genre_map[content_type][g_id]
+	                    g_names.append(g_name)
+	                items_extract[idx].pop('genre_ids', None)
+	                items_extract[idx]['genres'] = g_names
+	            
+	            #Convert rating from 10-scale to 5-scale
+	            if m['vote_average'] == 0: pass
+	            else:
+	                avg_adj = str(round(m['vote_average']/2, 2))
+	                avg_adj_str = f'{avg_adj}/5'
+	                items_extract[idx]['vote_average'] = avg_adj_str
+	            
+	        self.doc['search_result'] = items_extract
+	        
+	#DRIVER
+	content_type = "movie" #TODO: Extract from url that XHR will construct
+	search_terms = "dark knight" #TODO: Extract from url that XHR will construct
+	# search_terms = "joker"
+
+	sp = SearchPage()
+	sp.get_content(search_terms, content_type)
+	search_doc_resp = flask.jsonify(sp.doc)
+	search_doc_resp.headers.add('Access-Control-Allow-Origin', '*')
+	return search_doc_resp
+
 
 ########## RENDER TEMPLATE ########## 
-@app.route('/hello/<name>')
-def hello(name=None):
+# @app.route('/hello/<name>')
+# def hello(name=None):
 
-	#Pass in a single string
-	# return render_template('hello.html', name=name)
+# 	#Pass in a single string
+# 	# return render_template('hello.html', name=name)
 
-	#Pass in a json object
-	url_raw = url_for('hello', name=name)
-	resp_dict = {"query_string": url_raw,
-				"username": name}
-	resp_j = json.dumps(resp_dict)
-	return render_template('hello.html', name=resp_j)
+# 	#Pass in a json object
+# 	url_raw = url_for('hello', name=name)
+# 	resp_dict = {"query_string": url_raw,
+# 				"username": name}
+# 	resp_j = json.dumps(resp_dict)
+# 	return render_template('hello.html', name=resp_j)
 
 
 
